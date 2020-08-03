35b61cea588f71ea55cc769ef0b68c1e821e0b99
haifeng-x <404109455@qq.com>
2020-08-03 15:58:44 +0800

    remove pyworkspace and pyqtworkspace

--- pyworkspace/zte/defile.py
+++
@@ -1,6 +0,0 @@
-import os
-
-for file in os.listdir('D:\ZTE\ZTEdev\Bluetooth'):
-    fsize = os.path.getsize(f'D:\ZTE\ZTEdev\Bluetooth\{file}')
-    if(fsize<1000000):
-        os.remove(f'D:\ZTE\ZTEdev\Bluetooth\{file}')
--- pyworkspace2/stringtest.py
+++
@@ -1,4 +0,0 @@
-dest='ju'
-count=1
-d=dest+str(count)+'.pdf'
-print(d)
