be8d717e5c1be1f4a69e7a080e28060ea4643f8e
haifeng-x <404109455@qq.com>
2020-08-21 22:14:37 +0800

    brightness can‘t auto down

--- androidworkspace/Mysecondkotlin/app/src/main/java/com/example/mysecondkotlin/BrightnessActivity.kt
+++ androidworkspace/Mysecondkotlin/app/src/main/java/com/example/mysecondkotlin/BrightnessActivity.kt
@@ -3,13 +3,15 @@ package com.example.mysecondkotlin
 import android.app.Activity
 import android.content.Context
 import android.os.Bundle
+import android.os.Handler
 import android.util.Log
 import android.view.View
 import android.widget.SeekBar
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_brightness.*
-
+import java.util.*
+import kotlin.concurrent.schedule
 private const val RQ_WRITE_SETTINGS = 100
 class BrightnessActivity : AppCompatActivity() {
     protected lateinit var mContext: Context
@@ -35,34 +37,42 @@ class BrightnessActivity : AppCompatActivity() {
         Bright_atuodown.setOnClickListener(object : View.OnClickListener {
             override fun onClick(v: View?) {
 
-                Toast.makeText(v?.context, "brihtdownclick", Toast.LENGTH_LONG).show();
+                Toast.makeText(v?.context, "brihtdownclick", Toast.LENGTH_SHORT).show();
+                //brightdown()
+                while (windowBrightness > 0.1F) {
 
-                while (windowBrightness > 0.1) {
-                    windowBrightness = windowBrightness - 0.1F
                     Log.d(this@BrightnessActivity.toString(), windowBrightness.toString())
                     tvWindowBright.text = "当前窗口亮度=$windowBrightness"
-                    Log.d(this@BrightnessActivity.toString(), tvWindowBright.text .toString())
+
+
+//                    Timer().schedule(1000){
+//                        Log.d(this@BrightnessActivity.toString(), tvWindowBright.text .toString())
+//                        windowBrightness -= 0.1F
+//                    }
+
                     try {
                         Thread.sleep(500)
+                        Log.d(this@BrightnessActivity.toString(), tvWindowBright.text .toString())
+                        windowBrightness -= 0.1F
                     } catch (e: Exception) {
                         e.printStackTrace()
                     }
 
-
+                    sbWindowBright.progress = (windowBrightness * 100).toInt()
                 }
-                if (windowBrightness > 0.0 && windowBrightness < 0.1)
+                if (windowBrightness > 0.0F && windowBrightness <= 0.1F){
                     windowBrightness = 0F
-
-                //windowBrightness = progress.toFloat() / 100F
-
-                tvWindowBright.text = "当前窗口亮度=$windowBrightness"
+                    tvWindowBright.text = "当前窗口亮度=$windowBrightness"
+                    sbWindowBright.progress = (windowBrightness * 100).toInt()}
             }
         })
 
     }
 
-    fun brightdown(){
-        windowBrightness -= 0.1F
+    fun brightdownstep(){
+        if (windowBrightness >=  0.0F && windowBrightness < 0.1F)
+            windowBrightness = 0F
+        else  windowBrightness -= 0.1F
     }
     var Activity.windowBrightness
         get() = window.attributes.screenBrightness
